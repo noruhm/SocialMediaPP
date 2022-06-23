e75215a5c0906e950e7f6689a645955bef5e8757
noruhm <henryhrty@gmail.com>
2022-06-23 15:59:11 +0700

    addpost, home sementara

--- routers/index.js
+++ routers/index.js
@@ -3,4 +3,14 @@ const Controller= require('../controllers')
 
 router.get('/', Controller.landingPage)
 
+router.get('/home', Controller.showAllPost)
+
+router.get('/profile/', Controller.profile)
+
+router.get('/addPost', Controller.formAddPost)
+
+router.post('/addPost', Controller.addPost)
+
+
+
 module.exports=router
